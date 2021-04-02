fb8fd47488215323c15e1ac5306a22b4c0085135
Denis Zaratan Pasin <zaratan@hey.com>
2021-04-02 15:22:26 +0900

    First init version

---
+++ lib/dev_oops/version.rb
@@ -0,0 +1,3 @@
+module DevOops
+  VERSION = "0.1.0"
+end
---
+++ spec/dev_oops_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe DevOops do
+  it "has a version number" do
+    expect(DevOops::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
